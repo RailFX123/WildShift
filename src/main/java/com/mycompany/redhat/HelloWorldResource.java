ac577666ec32659fc2bc79021b3a611f59be1fa4
David Natareno <gezio111@gmail.com>
2020-01-22 12:10:36 -0600

    Primera Prueba ambiente corriendo

--- src/main/java/com/mycompany/redhat/HelloWorldResource.java
+++ src/main/java/com/mycompany/redhat/HelloWorldResource.java
@@ -39,7 +39,7 @@ public class HelloWorldResource {
     @Produces(MediaType.APPLICATION_JSON)
     public String getJson() {
         //TODO return proper representation object
-        return "{\"Message\":\"Hello World4\"}";
+        return "{\"Message\":\"Hello World12\"}";
     }
 
     /**
